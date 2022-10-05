8a48b6178be41e6da4f19fe253c32d4626ac6064
Juan Manuel Fernandez Casenave <juanmafc22@gmail.com>
2022-10-05 20:06:53 -0300

    Commit final Mercadoliebre

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const { redirect } = require("express/lib/response");
+const app = express();
+const puertoExpress = 3030;
+
+const path = require("path");
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.set("view engine", "ejs");
+
+
+app.listen(process.env.PORT || puertoExpress, () => {
+    console.log(`Express iniciado OK el el puerto ${puertoExpress}`);
+});
+
+app.get("/", (req, res) => {
+    res.render("home");
+    // res.sendFile(path.resolve (__dirname, "./views/home.ejs"))
+});
+
+app.get("/home", (req, res) => {
+    res.render("home");
+});
+
+app.post("/home", (req, res) => {
+    res.render("home")
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.resolve (__dirname, "./views/home.html"))
+// });
+
+// app.get("/home", (req, res) => {
+//     res.sendFile(path.resolve (__dirname, "./views/home.html"))
+// });
+
+// app.post("/home", (req, res) => {
+//     res.sendFile(path.resolve (__dirname, "./views/home.html"))
+// });
+
+// app.get("/register", (req, res) => {
+//     res.sendFile(path.resolve (__dirname, "./views/register.html"))
+// });
+
+// app.get("/login", (req, res) => {
+//     res.sendFile(path.resolve (__dirname, "./views/login.html"))
+// });
