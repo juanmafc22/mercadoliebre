6dcfd9cf07e10b0e61113d1407d214f9e31021e0
Juan Manuel Fernández <juanmafc22@gmail.com>
2022-10-07 18:35:39 -0300

    Home cambiado por Index. Links al index arreglados

--- app.js
+++ app.js
@@ -1,30 +1,51 @@
+// ******************** express () 
+//
 const express = require("express");
-const { redirect } = require("express/lib/response");
 const app = express();
 const puertoExpress = 3030;
-
-const path = require("path");
-const publicPath = path.resolve(__dirname, "./public");
-app.use(express.static(publicPath));
-
-app.set("view engine", "ejs");
+// const { redirect } = require("express/lib/response");
 
 
+// ******************** server start
+//
 app.listen(process.env.PORT || puertoExpress, () => {
     console.log(`Express iniciado OK el el puerto ${puertoExpress}`);
 });
 
+
+// ******************** requires
+//
+const path = require("path");
+const publicPath = path.resolve(__dirname, "./public");
+
+// ******************** middlewares (app.use)
+//
+app.use(express.static(publicPath));
+
+// ******************** template engines (app.set, carpeta views)
+//
+app.set("view engine", "ejs");
+
+// ******************** required route system
+//
+
+
+
+
 app.get("/", (req, res) => {
-    res.render("home");
-    // res.sendFile(path.resolve (__dirname, "./views/home.ejs"))
+    res.render("index");
 });
 
-app.get("/home", (req, res) => {
-    res.render("home");
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-app.post("/home", (req, res) => {
-    res.render("home")
+app.get("/index", (req, res) => {
+    res.render("index");
+});
+
+app.post("/index", (req, res) => {
+    res.render("index")
 });
 
 app.get("/register", (req, res) => {
@@ -35,23 +56,3 @@ app.get("/login", (req, res) => {
     res.render("login");
 });
 
-
-// app.get("/", (req, res) => {
-//     res.sendFile(path.resolve (__dirname, "./views/home.html"))
-// });
-
-// app.get("/home", (req, res) => {
-//     res.sendFile(path.resolve (__dirname, "./views/home.html"))
-// });
-
-// app.post("/home", (req, res) => {
-//     res.sendFile(path.resolve (__dirname, "./views/home.html"))
-// });
-
-// app.get("/register", (req, res) => {
-//     res.sendFile(path.resolve (__dirname, "./views/register.html"))
-// });
-
-// app.get("/login", (req, res) => {
-//     res.sendFile(path.resolve (__dirname, "./views/login.html"))
-// });
