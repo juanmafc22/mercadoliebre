f80ec05abca3689d6749ff7b5b56ac9b1b8dda0c
Juan Manuel Fernández <juanmafc22@gmail.com>
2022-10-10 12:38:41 -0300

    Trabajo hasta el radio button del form de register

--- public/js/register-form-validator.js
+++ public/js/register-form-validator.js
@@ -1,12 +1,16 @@
 const form = document.getElementById("form");
+const fullname = document.getElementById("fullname");
 const username = document.getElementById("username");
+const email = document.getElementById("email");
+const typeOfUser = document.getElementById("typeOfUser");
 const password = document.getElementById("password");
+
 
 // Show error messages
 function showError(input, message) {
     const formControl = input.parentElement;
     formControl.className = "form-control error";
-    const small = formControl.querySelector('small');
+    const small = formControl.querySelector("small");
     small.innerText = message
 }
 
@@ -16,25 +20,55 @@ function showSuccess(input) {
     formControl.className = "form-control success";
 }
 
+// Show valid email
+function isValidEmail(email) {
+    const re = /^.+@.+\..+$/;
+    return re.test(String(email).toLowerCase());
+}
+
 // event listener for the submit of the forms
-form.addEventListener('submit', function(event) {
+form.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    if (username.value === '') {
-        showError(username, 'Se requiere nombre de usuario, no puede estar vacío')
-    } else if (username.value.length > 0 && username.value.length < 8) {
-        showError(username, 'El nombre de usuario debe tener por lo menos 8 caracteres')
-    }
-    else {
-        showSuccess(username);
+    if (fullname.value === "") {
+        showError(fullname, "Se requiere su nombre completo");
+    } else if (fullname.value.length > 0 && fullname.value.length < 5) {
+        showError(fullname, `No sea tramposo, de verdad su nombre completo solo tiene ${fullname.value.length} caracteres?`);
+    } else {
+        showSuccess(fullname);
     }
 
-    if (password.value === '') {
-        showError(password, 'Debe ingresar su contraseña')
-    } else if (password.value.length > 0 && password.value.length < 8) {
-        showError(password, 'La contraseña debe tener por lo menos 8 caracteres')
+    
+    if (username.value === "") {
+        showError(username, "Se requiere nombre de usuario")
+    } else if (username.value.length > 0 && username.value.length < 8) {
+        showError(username, "El nombre de usuario debe tener por lo menos 8 caracteres")
+    } else {
+        showSuccess(username);
     }
-    else {
+    
+
+    if (email.value === "") {
+        showError(email, "Se requiere una dirección de email")
+    } else if (!isValidEmail(email.value)) {
+        showError(email, "La dirección de email no es válida")
+    } else {
+        showSuccess(email);
+    }
+
+    if (birthdate.value === "") {
+        showError(birthdate, "Se requiere una fecha de nacimiento");
+    } else {
+        showSuccess(birthdate);
+    }
+
+
+    if (password.value === "") {
+        showError(password, "Debe ingresar su contraseña")
+    } else if (password.value.length > 0 && password.value.length < 8) {
+        showError(password, "La contraseña debe tener por lo menos 8 caracteres")
+    } else {
         showSuccess(password);
     }
+    
 });
