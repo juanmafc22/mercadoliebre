f92dc55a5a98eb38a056f9036ee93a4855e995e3
Juan Manuel Fernández <juanmafc22@gmail.com>
2022-10-19 13:21:01 -0300

    Modified app.js

--- app.js
+++ app.js
@@ -1,50 +1,41 @@
-// ******************** express () 
-//
+
+// ------- | requires
 const express = require("express");
+const mainRouter = require("./routes/main.router");
+const usersRouter = require("./routes/users.router");
+const path = require("path");
+const publicPath = path.resolve(__dirname, "./public");
+
+
+// ------- | express () 
 const app = express();
 const puertoExpress = 3000;
 // const { redirect } = require("express/lib/response");
 
 
-// ******************** server start
-//
+// ------- | server start
 app.listen(process.env.PORT || puertoExpress, () => {
     console.log(`Express iniciado OK el el puerto ${puertoExpress}`);
     console.log("Ir al sitio Web: http://localhost:" + puertoExpress + "/");
 });
 
 
-// ******************** requires
-//
-const path = require("path");
-const publicPath = path.resolve(__dirname, "./public");
-const mainRouter = require("./routes/main.router");
-const usersRouter = require("./routes/users.router");
-const pruebasRouter = require("./routes/pruebas.router");
-
-
-// ******************** middlewares (app.use)
-//
+// ------- | middlewares (app.use)
 app.use(express.static(publicPath));
 app.use(express.urlencoded({ extended:false } ));
 app.use(express.json());
 
 
-// ******************** template engines (app.set, carpeta views)
-//
+// ------- | template engines (app.set, carpeta views)
 app.set("view engine", "ejs");
 
 
-// ******************** required route system
-//
+// ------- | required route system
 app.use("/", mainRouter);
-
 app.use("/users", usersRouter);
 
-app.use("/pruebas", pruebasRouter);
 
-
-// Seteo inicial de error 404
+// ------- | seteo inicial del error 404 
 app.use((req, res, next) => {
     res.status(404).send("Page not found");
 });
